b782c9f553da94610c77959f11f6f5cd0d57eff6
Matt Grimes <matt@mtgrimes.com>
2023-11-30 22:22:33 -0700

    Day1 complete

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,29 @@
 use crate::utils::*;
 
-fn get_digits(string: &String) -> Vec<u32> {
-    let mut digits: Vec<u32> = vec![];
-    for character in string.chars() {
+const NUMBERS: &'static [&'static str; 10] = &[
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn find_text_number(string: &str) -> Option<usize> {
+    for (i, &text) in NUMBERS.iter().enumerate() {
+        if text.len() <= string.len() {
+            if string[0..text.len()] == *text {
+                return Some(i);
+            }
+        }
+    }
+    None
+}
+
+fn get_digits(string: &String) -> Vec<usize> {
+    let mut digits: Vec<usize> = vec![];
+    for (i, character) in string.chars().enumerate() {
         match character.to_digit(10) {
-            Some(digit) => digits.push(digit),
-            None => (),
+            Some(digit) => digits.push(digit as usize),
+            None => match find_text_number(&string[i..]) {
+                Some(digit) => digits.push(digit),
+                None => (),
+            },
         }
     }
     digits
@@ -13,7 +31,7 @@ fn get_digits(string: &String) -> Vec<u32> {
 
 pub fn day1(input_file: String) {
     let lines = parse_file(input_file);
-    let digits: u32 = lines
+    let digits: usize = lines
         .iter()
         .map(get_digits)
         .map(|digits| digits[0] * 10 + digits.last().unwrap())
