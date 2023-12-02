a61570250d52a1c04e714bdd421abff1e1fd8a46
Matt Grimes <matt@mtgrimes.com>
2023-12-02 15:53:33 -0700

    Part 1 done

--- src/aoc2015/day1.rs
+++ src/aoc2015/day1.rs
@@ -1,6 +1,23 @@
 use crate::utils::*;
 pub fn day1(input_file: &str) -> [usize; 2] {
-    let lines = parse_file(input_file);
-    println!("{:?}", lines);
-    [0, 0]
+    let lines = parse_file(input_file).pop().unwrap();
+    let mut l_count = 0_usize;
+    let mut r_count = 0_usize;
+    for c in lines.chars() {
+        match c {
+            '(' => l_count += 1,
+            ')' => r_count += 1,
+            _ => todo!(),
+        }
+    }
+    [l_count - r_count, 0]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part1() {
+        assert_eq!(day1("inputs/2015/day1.txt")[0], 280);
+    }
 }
